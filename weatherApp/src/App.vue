b19ba40f9d1916dc0f1cf250d81f17f321bb989f
Vitaly Savinov <vitalanother@gmail.com>
2025-03-19 09:06:37 +0300

    made some changes

--- weatherApp/src/App.vue
+++ weatherApp/src/App.vue
@@ -2,6 +2,23 @@
 import { RouterLink, RouterView } from 'vue-router'
 import DropDown from "@/components/DropDown.vue";
 import {ref} from "vue";
+import {useForecastStore} from "@/stores/forecast.ts";
+
+const forecast = useForecastStore();
+
+const cityList = ([
+  {name: 'Казань', latitude: 55.7887, longitude: 49.1221,},
+  {name: 'Краснодар', latitude: 45.0448, longitude: 38.976,},
+  {name: 'Уфа', latitude: 54.7431, longitude: 55.9678,},
+  {name: 'Новосибирск', latitude: 55.0415, longitude: 82.9346,},
+  {name: 'Красноярск', latitude: 56.0184, longitude: 92.8672,},
+  {name: 'Москва', latitude: 55.7522, longitude: 37.6156,}
+])
+forecast.setCities(cityList);
+
+
+
+
 
 const options = ref([
   {name: 'Казань', value: 1,},
--- weatherApp/src/stores/forecast.ts
+++ weatherApp/src/stores/forecast.ts
@@ -1,27 +1,40 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import {apiResponse} from "@/api/weatherApi.ts";
 
-export const useForecast = defineStore('forecast', () => {
-  const cities = ref();
-  const apiWeather = ref();
-  // const doubleCount = computed(() => count.value * 2)
-  const allWeather = ref();
-  const cityWeather = ref();
-  function increment() {
-    count.value++
+export const useForecastStore = defineStore('forecast', () => {
+
+  // список городов. его я буду получать 1 раз при создании приложения. Там названия городов и их координаты(для API)
+  const cities = ref(null);
+
+  // голый ответ API - массив с объектами городами и всей доступной информацией о них
+  const apiWeather = ref(null);
+
+  // новый массив с прогнозами. результат слияния cities и apiWeather. содержит только необходимые данные о погоде и русские названия городов.
+  const allWeather = computed(()=> cities.value.map((item, index) => {
+    return {
+      name: item.name,
+      current: apiWeather[index].current,
+      hourly: apiWeather[index].hourly
+    };
+  }))
+
+  // выбранный город. внутри один конкретный объект из массива allWeather
+  const cityWeather = ref(null);
+
+  // получаем список городов и их координат. на случай если он изменился.
+  function setCities(cityList) {
+    cities.value = cityList
   }
 
-  function getApiWeather() {
-
+  // получаем ответ API
+  async function getApiWeather() {
+    apiWeather.value = apiResponse();
+  }
+  // меняем значение cityWeather, когда выбран город
+  function setCityWeather(chosenCity) {
+    cityWeather.value = chosenCity
   }
 
-  function setApiWeather() {
-
-  }
-
-  function setCityWeather() {
-
-  }
-
-  return { count, doubleCount, increment }
+  return { cities, apiWeather, allWeather, cityWeather, setCities, getApiWeather, setCityWeather }
 })
